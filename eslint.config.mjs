36a8b0c486d6bef8a2bfa44843d35dc6c09143c3
appleex <771992522@qq.com>
2025-03-11 02:40:54 +0800

    build: 优化项目配置文件

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+import tsEslint from 'typescript-eslint'
+import globals from 'globals'
+
 /**
  * https://eslint.org/docs/latest/use/configure/migration-guide
  * https://eslint.org/docs/latest/use/configure/
@@ -5,25 +8,33 @@
  * Tip: eslint9 not support `.eslintignore`,
  * need to config `eslint.config.js` in `ignore` or `ignorePattern` options.
  */
-export default [
-    // Default configuration
-    {
-        name: 'global',
-        languageOptions: {
-            parserOptions: {
-                warnOnUnsupportedTypeScriptVersion: false
-            }
-        },
-        files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
-        rules: {},
-    },
+export default tsEslint.config(
     // Ignore
     {
         ignores: [
-            'node_modules',
-            'build',
-            'dist',
-            'docs',
+            '**/node_modules/**',
+            '**/dist/**',
+            '**/docs/**',
         ],
     },
-]
+    {
+        name: 'main',
+        languageOptions: {
+            parser: tsEslint.parser,
+            parserOptions: {
+                sourceType: 'module',
+                ecmaVersion: 2022,
+                warnOnUnsupportedTypeScriptVersion: false,
+                project: [
+                    './packages/*/tsconfig.json',
+                ]
+            },
+            globals: {
+                ...globals.es2021,
+                ...globals.node,
+            },
+        },
+        plugins: {},
+        rules: {},
+    }
+)
